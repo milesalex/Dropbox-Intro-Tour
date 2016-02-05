64da7aa1787a5b4e9b4c196e0e3773e578df30e8
Alex Miles <alexmiles@dropbox.com>
2016-02-05 12:08:17 -0800

    Add paginated scroll view

--- Dropbox Intro Tour/WelcomeViewController.swift
+++ Dropbox Intro Tour/WelcomeViewController.swift
@@ -1,24 +1,25 @@
 //
-//  SettingsViewController.swift
+//  WelcomeViewController.swift
 //  Dropbox Intro Tour
 //
-//  Created by Alex Miles on 2/3/16.
+//  Created by Alex Miles on 2/5/16.
 //  Copyright © 2016 Dropbox. All rights reserved.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-    @IBOutlet weak var settingsScrollView: UIScrollView?
-    
+class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.settingsScrollView?.contentSize = CGSizeMake(320, 772)
+        self.scrollView.contentSize = CGSize(width: 960, height: 568)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 
